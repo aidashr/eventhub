015e107ec591eced4d838740b7a9872366e10650
Masih <bahmani.masih@gmail.com>
2020-11-07 18:46:19 +0330

    added is_private field to models.py and related serializers

---
+++ Backend/EventHubApp/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from .api import RegisterAPI, RegisterAPI2, LoginAPI
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('api/auth', include('knox.urls')),
+    path('api/user/signup', RegisterAPI.as_view()),
+    path('api/cafe/signup', RegisterAPI2.as_view()),
+    path('api/all-users/login', LoginAPI.as_view()),
+    path('api/all-users/information', views.profile_view),
+    path('api/all-users/password-update', views.ChangePasswordView.as_view(), name='password-update'),
+    path('api/main-page', views.get_cafes)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
