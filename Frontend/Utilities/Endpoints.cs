1c3a1ee567d68855889c4509007a78b88ed7d53d
Sahand Nazarzadeh <sahand.nazarzadeh@gmail.com>
2020-11-08 11:59:33 +0330

    Add Endpoints.cs to ./Utilities

---
+++ Frontend/Utilities/Endpoints.cs
@@ -0,0 +1,9 @@
+namespace Frontend.Pages
+{
+    public static class Endpoints
+    {
+        public static string Login => "api/all-users/login";
+        public static string UserSignup => "api/user/signup";
+        public static string CafeSignup => "api/cafe/signup";
+    }
+}
