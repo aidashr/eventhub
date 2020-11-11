dc3d55d9054c229f43646441020c4d48d09fffcc
Sahand Nazarzadeh <sahand.nazarzadeh@gmail.com>
2020-11-11 20:03:03 +0330

    Fix tab view of signup.razor

--- Frontend/Pages/Signup.razor.cs
+++ Frontend/Pages/Signup.razor.cs
@@ -10,16 +10,13 @@ using Microsoft.AspNetCore.Components.Routing;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 using Microsoft.JSInterop;
-using frontend;
-using frontend.Shared;
+using Frontend;
+using Frontend.Shared;
 
-namespace frontend.Pages
+namespace Frontend.Pages
 {
     public partial class Signup
     {
-         
-        private const string ACTIVE_TAB_CLASS = "nav-link font-weight-bold active";
-        private const string DEACTIVE_TAB_CLASS = "nav-link font-weight-bold";
         protected string FirstName { get; set; }
         protected string LastName { get; set; }
         protected string CafeName { get; set; }
@@ -29,33 +26,10 @@ namespace frontend.Pages
         protected string Password { get; set; }
         protected string ConfirmPassword { get; set; }
         protected bool IsCafeActive { get; set; }
-        protected string CafeTabClass { get; set; }
-        protected string UserTabClass { get; set; }
-
-        protected override void OnInitialized()
-        {
-            base.OnInitialized();
-
-            IsCafeActive = true;
-            CafeTabClass = ACTIVE_TAB_CLASS;
-            UserTabClass = DEACTIVE_TAB_CLASS;
-        }
-        protected void OnCafeTabClickMethod(EventArgs e)
-        {
-            IsCafeActive = true;
-            CafeTabClass = ACTIVE_TAB_CLASS;
-            UserTabClass = DEACTIVE_TAB_CLASS;
-        }
-        protected void OnUserTabClickMethod(EventArgs e)
-        {
-            IsCafeActive = false;
-            CafeTabClass = DEACTIVE_TAB_CLASS;
-            UserTabClass = ACTIVE_TAB_CLASS;
-        }
 
         protected async Task OnSignupClickMethod(EventArgs e)
         {
         }
-    
+
     }
 }
