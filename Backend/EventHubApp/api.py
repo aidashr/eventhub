b17c6f00019177b6622b88b29e37d83bca50af05
Masih Bahmani <bahmani.masih@gmail.com>
2020-11-27 18:59:53 +0000

    Fix/code cleanup

--- Backend/EventHubApp/api.py
+++ Backend/EventHubApp/api.py
@@ -1,7 +1,7 @@
-from rest_framework import generics, permissions, status
+from rest_framework import generics
 from rest_framework.response import Response
 from knox.models import AuthToken
-from .serializers import UserSerializer, RegisterSerializer, RegisterSerializer2, UserSerializer2, LoginSerializer, \
+from .serializers import UserSerializer, UserRegisterSerializer, CafeRegisterSerializer, CafeSerializer, LoginSerializer, \
     EventSerializer
 
 
@@ -29,7 +29,7 @@ class RegisterAPI(generics.GenericAPIView):
 
     def post(self, request, *args, **kwargs):
         if request.data.get('RegularUser'):
-            serializer = RegisterSerializer(data=request.data)
+            serializer = UserRegisterSerializer(data=request.data)
             serializer.is_valid(raise_exception=True)
             user = serializer.save()
             return Response({
@@ -38,40 +38,19 @@ class RegisterAPI(generics.GenericAPIView):
             })
 
         else:
-            serializer = RegisterSerializer2(data=request.data)
+            serializer = CafeRegisterSerializer(data=request.data)
             serializer.is_valid(raise_exception=True)
             user = serializer.save()
             return Response({
-                "user": UserSerializer2(user, context=self.get_serializer_context()).data,
+                "user": CafeSerializer(user, context=self.get_serializer_context()).data,
                 "token": AuthToken.objects.create(user)[1]
             })
-
-
-class UserAPI(generics.RetrieveAPIView):
-    permission_classes = [
-        permissions.IsAuthenticated,
-    ]
-    serializer_class = UserSerializer
-
-    def get_object(self):
-        return self.request.user
-
-
-class UserAPI2(generics.RetrieveAPIView):
-    permission_classes = [
-        permissions.IsAuthenticated,
-    ]
-    serializer_class = UserSerializer2
-
-    def get_object(self):
-        return self.request.user
 
 
 class LoginAPI(generics.GenericAPIView):
     serializer_class = LoginSerializer
 
     def get(self, request, *args, **kwargs):
-        new_data = request.data
         new_data = {'username': request.query_params.get('username'),
                     'password': request.query_params.get('password')
                     }
--- Backend/EventHubApp/models.py
+++ Backend/EventHubApp/models.py
@@ -3,20 +3,18 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    phone = models.IntegerField(null=True, unique=True)
+    phone_number = models.IntegerField(null=True, unique=True)
     cafe_name = models.CharField(null=True, max_length=100)
     email = models.EmailField(null=True, max_length=254, unique=True)
-    RegularUser = models.BooleanField(default=False)
-    CafesUser = models.BooleanField(default=False)
+    is_regular = models.BooleanField(default=False)
     profile_image = models.ImageField(blank=True, null=True)
     cafe_address = models.CharField(null=True, max_length=500)
     is_private = models.BooleanField(default=False)
 
 
 class Event(models.Model):
-    name = models.CharField(null=True, max_length=50)
+    title = models.CharField(null=True, max_length=50)
     description = models.CharField(null=True, max_length=450)
     created_at = models.DateTimeField(auto_now=True, auto_now_add=False)
     image = models.ImageField(blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-
