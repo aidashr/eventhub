2cfcfba0460e671d1d7e69424ecc4260779c8bb0
Sahand Nazarzadeh <sahand.nazarzadeh@gmail.com>
2020-12-02 15:39:56 +0330

    Change frontend framework to react js and change .gitignore file

--- Frontend/Utilities/Endpoints.cs
+++
@@ -1,9 +0,0 @@
-namespace Frontend.Pages
-{
-    public static class Endpoints
-    {
-        public static string Login => "/Users/login";
-        public static string UserSignup => "Users/regular/sign-up";
-        public static string CafeSignup => "Users/cafe/sign-up";
-    }
-}
