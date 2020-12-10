1cd96415b0a43bb38c6c3d34f151c58a59adc63d
Masih Bahmani <bahmani.masih@gmail.com>
2020-12-10 13:06:26 +0000

    Feature/events participation back

--- Backend/EventHubApp/urls.py
+++ Backend/EventHubApp/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('cafe/lastest', views.get_cafes),
     path('event/lastest', views.get_events),
     path('event/<int:id>', EventAPI.as_view()),
-    # path('event/<int:id>/participants',  views.ParticipanatsAPI.as_view()),
+    path('event/<int:id>/participants',  views.ParticipantsAPI.as_view()),
     path('event/search', views.SearchEvent.as_view()),
     path('cafe/search', views.SearchCafe.as_view()),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
