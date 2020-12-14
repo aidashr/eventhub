dc0520eb4f26bae6b1aedd3d1da8bba6ea293023
Masih Bahmani <bahmani.masih@gmail.com>
2020-12-14 14:48:27 +0000

    Fix/security update

--- Backend/EventHubApp/permissions.py
+++ Backend/EventHubApp/permissions.py
@@ -1,11 +1,36 @@
 from rest_framework import permissions
-from .models import User
+from .models import User, Event
 
 
 class IsOwner(permissions.BasePermission):
     def has_permission(self, request, view, **kwargs):
         try:
             user = User.objects.get(id=view.kwargs.get('id'))
+        except:
+            return self.check_request(request=request, view=view)
+
+        if not request.user.username == user.username:
+            return False
+
+        else:
+            return True
+
+    def check_request(self, request, view):
+        try:
+            user = User.objects.get(id=request.data.get('user'))
+        except:
+            return self.check_event_owner(request=request, view=view)
+
+        if not request.user.username == user.username:
+            return False
+
+        else:
+            return True
+
+    def check_event_owner(self, request, view):
+        try:
+            event = Event.objects.get(id=view.kwargs.get('id'))
+            user = event.user
         except:
             return False
 
@@ -14,3 +39,23 @@ class IsOwner(permissions.BasePermission):
 
         else:
             return True
+
+
+class IsPostRequest(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.method == "POST"
+
+
+class IsPutRequest(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.method == "PUT"
+
+
+class IsGetRequest(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.method == "GET"
+
+
+class IsDeleteRequest(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.method == "DELETE"
