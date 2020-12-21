7fe2fed5564e5640df4a5131e062a7c7e3da21eb
Masih <bahmani.masih@gmail.com>
2020-12-21 20:07:45 +0330

    Restore lost files in previous merges

--- Backend/EventHubApp/views.py
+++ Backend/EventHubApp/views.py
@@ -15,6 +15,168 @@ from .models import User, Event, Participation, CafeFollow
 from .permissions import IsOwner, IsPostRequest, IsPutRequest, IsDeleteRequest, IsGetRequest
 
 
+class CafeEventsAPI(generics.GenericAPIView):
+    def get(self, request, **kwargs):
+        try:
+            if len(request.query_params) == 0:
+                try:
+                    events = Event.objects.filter(user=User.objects.get(id=kwargs.get("id")))
+                    ser = EventSerializer(events, many=True)
+                    return Response(ser.data, status=status.HTTP_200_OK)
+
+                except:
+                    return Response(status=status.HTTP_404_NOT_FOUND)
+
+            from_time = request.query_params.get("from")
+            from_time = datetime.strptime(from_time, '%Y-%m-%dT%H:%M:%S')
+
+            to_time = request.query_params.get("to")
+            to_time = datetime.strptime(to_time, '%Y-%m-%dT%H:%M:%S')
+
+            try:
+                events = Event.objects.filter(user_id=kwargs.get('id'))
+                event_ser = EventSerializer(events, many=True)
+            except:
+                return Response(status=status.HTTP_404_NOT_FOUND)
+
+            new_event = event_ser.data
+
+            counter = 0
+            for i in range(len(new_event)):
+                event_time = str(new_event[i - counter].get('start_time')).split('+')[0]
+                event_time = datetime.strptime(event_time, '%Y-%m-%dT%H:%M:%S')
+
+                if not to_time >= event_time >= from_time:
+                    new_event.pop(i - counter)
+                    counter += 1
+
+            return Response(new_event, status=status.HTTP_200_OK)
+
+        except NameError:
+            print(NameError)
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class RemoveFollowerAPI(generics.GenericAPIView):
+    permission_classes = (IsAuthenticated, IsOwner)
+
+    def delete(self, request, **kwargs):
+        try:
+            CafeFollow.objects.filter(id=kwargs.get('follower')).delete()
+            return Response(status=status.HTTP_200_OK)
+
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+class GetFollowingsAPI(generics.GenericAPIView):
+    def get(self, request, **kwargs):
+        try:
+            user = User.objects.get(id=kwargs.get('id'))
+            followings = CafeFollow.objects.filter(follower=user)
+            ser = CafeFollowSerializer(followings, many=True)
+            return Response(ser.data, status=status.HTTP_200_OK)
+
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+class GetFollowersAPI(generics.GenericAPIView):
+    def get(self, request, **kwargs):
+        try:
+            user = User.objects.get(id=kwargs.get('id'))
+            followers = CafeFollow.objects.filter(followed=user)
+            ser = CafeFollowSerializer(followers, many=True)
+            return Response(ser.data, status=status.HTTP_200_OK)
+
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+class CafeFollowAPI(generics.GenericAPIView):
+    permission_classes = (IsAuthenticated, IsOwner)
+
+    def post(self, request, **kwargs):
+        new_data = {'is_regular': kwargs.get('id'),
+                    'cafe': request.data.get('cafe')
+                    }
+        serializer = CafeFollowSerializer(data=new_data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, **kwargs):
+        try:
+            CafeFollow.objects.filter(follower=kwargs.get('id'), followed=kwargs.get('cafe')).delete()
+            return Response(status=status.HTTP_200_OK)
+
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+class ParticipantsAPI(generics.GenericAPIView):
+    queryset = Participation.objects.all()
+    serializer_class = ParticipateSerializer
+    permission_classes = [Or(And(IsGetRequest, AllowAny),
+                             And(IsPostRequest, IsOwner),
+                             And(IsDeleteRequest, IsOwner))]
+
+    def get(self, request, **kwargs):
+        post = Participation.objects.filter(event_id=kwargs.get('event_id'))
+        serializer = ParticipateSerializer(post, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, **kwargs):
+        new_data = {'event': kwargs.get('event_id'),
+                    'user': request.data.get('user')
+                    }
+
+        serializer = PostParticipateSerializer(data=new_data)
+
+        event = Event.objects.get(id=new_data.get('event'))
+
+        if event.capacity <= event.participants_count:
+            return Response({"error": "Can't accept more participants"}, status=status.HTTP_400_BAD_REQUEST)
+
+        if serializer.is_valid():
+            par = serializer.save()
+            data = {"participants_count": event.participants_count + 1}
+            update_ser = PostParticipateSerializer(event, data=data, partial=True)
+
+            if update_ser.is_valid():
+                update_ser.update(instance=event, validated_data=data)
+
+            return Response(ParticipateSerializer(par).data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, **kwargs):
+        try:
+            if Participation.objects.get(id=kwargs.get('participation_id')).event.id != kwargs.get('event_id'):
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+
+            Participation.objects.filter(id=kwargs.get('participation_id')).delete()
+            event = Event.objects.get(id=kwargs.get('event_id'))
+
+            data = {"participants_count": event.participants_count - 1}
+
+            if event.participants_count == 0:
+                data = {"participants_count": 0}
+
+            update_ser = PostParticipateSerializer(event, data=data, partial=True)
+
+            if update_ser.is_valid():
+                update_ser.update(instance=event, validated_data=data)
+
+            return Response(status=status.HTTP_200_OK)
+
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+
 @api_view(['GET', ])
 @permission_classes((AllowAny,))
 def get_past_events(request, **kwargs):
@@ -55,11 +217,11 @@ def get_future_events(request, **kwargs):
     return Response(new_event, status=status.HTTP_200_OK)
 
 
-@api_view(['GET', 'PUT'])
-@permission_classes((IsAuthenticated, ))
-# @permission_classes((IsOwner, IsAuthenticated))
-def profile_view(request, **kwargs):
-    if request.method == 'GET':
+class UserProfile(generics.GenericAPIView):
+    permission_classes = [Or(And(IsGetRequest, AllowAny),
+                             And(IsPutRequest, IsOwner))]
+
+    def get(self, request, **kwargs):
         try:
             user = User.objects.get(id=kwargs.get('id'))
 
