96754c0e99c3d222a98b66cd7badd93f47dcdfe6
MatinMarjani <matinmarjani@gmail.com>
2021-01-02 09:31:50 +0000

    Change id in events' url and other usages to event_id to prevent confliction between event_id and user_id in permissions.py

--- frontend/src/pages/Search.js
+++ frontend/src/pages/Search.js
@@ -1,149 +1,286 @@
-import { Component } from 'react';
-import React from "react";
-import { Redirect } from 'react-router';
+import { Component } from "react";
 import axios from "axios";
-import Cookies from 'universal-cookie';
-import './Search.css';
-import CafeSearch from "./../components/CafeSearch";
-import EventSearch from "./../components/EventSearch"
-
-
-
-import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
-import Avatar from 'react-avatar';
-import ListGroup from 'react-bootstrap/ListGroup'
-import Tabs from 'react-bootstrap/Tabs'
-import Tab from 'react-bootstrap/Tab'
-import Image from 'react-bootstrap/Image'
-
+import Rating from "@material-ui/lab/Rating";
+import Cafe from "./../components/Cafe";
+import "./Search.css";
+import "./../App.css";
 
 class Search extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isLoading: true,
-            searchText: '',
-            searchResults: [],
-            cafe: [],
-            listItems: [],
+  constructor(props) {
+    super(props);
 
-            event: [],
-            listItems1: [],
+    this.state = {
+      active_type: 3,
+      active_tag: 63,
+      cafes: [],
+      events: [],
+    };
+  }
 
-            image: 'https://c402277.ssl.cf1.rackcdn.com/photos/10596/images/carousel_small/RAWP2115_mini.jpg?1451407554',
+  componentDidMount() {
+    var queryParameters = new URLSearchParams(this.props.location.search);
+    var input = queryParameters.get("input");
 
-            mem_length: 0
-        };
+    axios
+      .get("http://127.0.0.1:8000/cafe/search", {
+        params: {
+          page: 1,
+          search: input,
+        },
+      })
+      .then(
+        function (response) {
+          this.setState({ cafes: response.data.results });
+        }.bind(this)
+      )
+      .catch(function (error) {
+        console.log(error);
+      })
+      .then(function () {});
 
+    axios
+      .get("http://127.0.0.1:8000/event/search", {
+        params: {
+          page: 1,
+          search: input,
+        },
+      })
+      .then(
+        function (response) {
+          this.setState({ events: response.data.results });
+        }.bind(this)
+      )
+      .catch(function (error) {
+        console.log(error);
+      })
+      .then(function () {});
+  }
 
-        this.handleChange = this.handleChange.bind(this);
-        //this.handleSubmit = this.handleSubmit.bind(this);
-    }
+  render() {
+    return (
+      <div className='background'>
+        <div className='container-md'>
+          <div className='main-container'>
+            <div className='filter-container'>
+              <div className='filter-content'>
+                <h5>Result Type</h5>
+                <div class='form-check ml-4'>
+                  <input
+                    class='form-check-input'
+                    type='checkbox'
+                    checked={1 === (this.state.active_type & 1)}
+                    onClick={() =>
+                      this.setState({ active_type: this.state.active_type ^ 1 })
+                    }
+                  />
+                  <label class='form-check-label'>Cafe</label>
+                </div>
+                <div class='form-check ml-4'>
+                  <input
+                    class='form-check-input'
+                    type='checkbox'
+                    checked={2 === (this.state.active_type & 2)}
+                    onClick={() =>
+                      this.setState({ active_type: this.state.active_type ^ 2 })
+                    }
+                  />
+                  <label class='form-check-label'>Event</label>
+                </div>
+              </div>
 
-    handleChange(event) {
-        this.setState({
-            [event.target.name]: event.target.value
-        })
-    }
-
-    componentDidMount() {
-
-        this.setState({ searchText: '' })
-
-        var url = 'http://127.0.0.1:8000/cafe/search'
-        axios.get(
-            url,
-            { params: { page: '1', search: this.state.searchText } }
-        ).then((res) => {
-            if (res.status === 200) {
-                this.setState({ cafe: res.data.results || [] })
-                this.setState({
-                    listItems: this.state.cafe.map((cafe) => (
-                        <CafeSearch key={cafe.id} cafe={cafe} />
-                    ))
-                })
-            }
-        }).catch((error) => {
-            if (error.response) {
-                console.log(error) // LOG
-            }
-        });
-
-        /////////////////////////////////////
-
-
-        var url1 = 'http://127.0.0.1:8000/event/search'
-        axios.get(
-            url1,
-            { params: { page: '1', search: this.state.searchText } }
-        ).then((res) => {
-            if (res.status === 200) {
-                this.setState({ event: res.data.results || [] })
-                this.setState({
-                    listItems1: this.state.event.map((event) => (
-                        <EventSearch key={event.id} event={event} />
-                    ))
-                })
-            }
-        }).catch((error) => {
-            if (error.response) {
-                console.log(error) // LOG
-            }
-        });
-
-    }
-
-    render() {
-        return (
-            <div>
-                <Tabs className='Search-Tabs' defaultActiveKey="home" variant="pills" className="mb-3" fill>
-
-                    <Tab eventKey="home" title="Cafes" className='Cafe-Tab'>
-                        <div >
-                            <Card className="Cafe-Search" >
-                                <div>
-                                    <h2>
-                                        Cafes Search Results:
-                                    </h2>
-                                </div>
-                                <Card.Body className="Search-Body">
-                                    <ListGroup>
-                                        {this.state.listItems}
-                                        {this.state.listItems}
-                                        {this.state.listItems}
-                                        {this.state.listItems}
-                                    </ListGroup>
-                                </Card.Body>
-                            </Card>
-                        </div >
-                    </Tab>
-                    <Tab eventKey="profile" title="Events">
-                        <div >
-                            <Card className="Cafe-Search">
-                                <div>
-                                    <h2>
-                                        Events Search Results :
-                                    </h2>
-                                </div>
-                                <Card.Body className="Search-Body">
-                                    <ListGroup>
-                                        {this.state.listItems1}
-                                        {this.state.listItems1}
-                                        {this.state.listItems1}
-                                        {this.state.listItems1}
-                                    </ListGroup>
-                                </Card.Body>
-                            </Card>
-                        </div >
-                    </Tab>
-                </Tabs>
+              {2 === (this.state.active_type & 2) ? (
+                <div className='filter-content'>
+                  <h5 className='mb-2'>Events</h5>
+                  <div className='mb-2'>
+                    <h6 className='mb-1'>Time</h6>
+                  </div>
+                  <div className='mb-2'>
+                    <h6 className='mb-1'>Price</h6>
+                  </div>
+                  <div className='mb-2'>
+                    <h6>Category</h6>
+                    <div class='form-check ml-4'>
+                      <input
+                        class='form-check-input'
+                        type='checkbox'
+                        checked={1 === (this.state.active_tag & 1)}
+                        onClick={() =>
+                          this.setState({
+                            active_tag: this.state.active_tag ^ 1,
+                          })
+                        }
+                      />
+                      <label class='form-check-label'>book review</label>
+                    </div>
+                    <div class='form-check ml-4'>
+                      <input
+                        class='form-check-input'
+                        type='checkbox'
+                        checked={2 === (this.state.active_tag & 2)}
+                        onClick={() =>
+                          this.setState({
+                            active_tag: this.state.active_tag ^ 2,
+                          })
+                        }
+                      />
+                      <label class='form-check-label'>movie review</label>
+                    </div>
+                    <div class='form-check ml-4'>
+                      <input
+                        class='form-check-input'
+                        type='checkbox'
+                        checked={4 === (this.state.active_tag & 4)}
+                        onClick={() =>
+                          this.setState({
+                            active_tag: this.state.active_tag ^ 4,
+                          })
+                        }
+                      />
+                      <label class='form-check-label'>game</label>
+                    </div>
+                    <div class='form-check ml-4'>
+                      <input
+                        class='form-check-input'
+                        type='checkbox'
+                        checked={8 === (this.state.active_tag & 8)}
+                        onClick={() =>
+                          this.setState({
+                            active_tag: this.state.active_tag ^ 8,
+                          })
+                        }
+                      />
+                      <label class='form-check-label'>board game</label>
+                    </div>
+                    <div class='form-check ml-4'>
+                      <input
+                        class='form-check-input'
+                        type='checkbox'
+                        checked={16 === (this.state.active_tag & 16)}
+                        onClick={() =>
+                          this.setState({
+                            active_tag: this.state.active_tag ^ 16,
+                          })
+                        }
+                      />
+                      <label class='form-check-label'>video game</label>
+                    </div>
+                    <div class='form-check ml-4'>
+                      <input
+                        class='form-check-input'
+                        type='checkbox'
+                        checked={32 === (this.state.active_tag & 32)}
+                        onClick={() =>
+                          this.setState({
+                            active_tag: this.state.active_tag ^ 32,
+                          })
+                        }
+                      />
+                      <label class='form-check-label'>sports events</label>
+                    </div>
+                  </div>
+                  <div className='d-flex flex-row'>
+                    <h6 className='mb-0'>rate</h6>
+                    <Rating className='ml-2' value={1} />
+                  </div>
+                </div>
+              ) : null}
             </div>
+            <div className='result-container'>
+              <div className='d-flex flex-row align-items-center border-bottom p-2'>
+                <small className='font-weight-bold mr-2'>Sort By</small>
+                <button className='btn btn-light btn-sm mr-2'>popular</button>
+                <button className='btn btn-light btn-sm mr-2'>comments</button>
+                <button className='btn btn-light btn-sm mr-2'>time</button>
+              </div>
+              <div className='p-2'>
+                {1 === (this.state.active_type & 1) ? (
+                  <div>
+                    {this.state.cafes.map((cafe) => (
+                      <div className='mb-2'>{this.getCafe(cafe)}</div>
+                    ))}
+                  </div>
+                ) : null}
+                {2 === (this.state.active_type & 2) ? (
+                  <div>
+                    {this.state.events.map((event) => (
+                      <div className='mb-2'>{this.getEvent(event)}</div>
+                    ))}
+                  </div>
+                ) : null}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
-        );
-    }
+  getEvent(event) {
+    console.log(event);
+    return (
+      <div style={{ borderRadius: "8px", border: "1px solid gray" }}>
+        <div className='row no-gutters bg-light p-2 align-items-center'>
+          <a href={"/profile/" + event.user.id}>
+            <img
+              className='shadow-lg border border-secondary rounded-circle mr-2'
+              src={event.user.profile_image}
+              width={48}
+              height={48}
+              alt='profile'
+            />
+          </a>
+          <a
+            href={"/profile/" + event.user.id}
+            className='text-decoration-none text-body font-weight-bold'>
+            {event.user.username}
+          </a>
+        </div>
+
+        {event.image ? (
+          <div className='row no-gutters w-100'>
+            <img className='d-block w-100' src={event.image} alt='event' />
+          </div>
+        ) : null}
+
+        <div className='row no-gutters bg-light p-2'>
+          <div className='col w-100'>
+            <div
+              className='row no-gutters'
+              data-toggle='tooltip'
+              data-placement='top'
+              title={"start at " + event.start_time}>
+              <h5>{event.title}</h5>
+            </div>
+            <div className='row no-gutters'>{event.description}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  getCafe(cafe) {
+    return (
+      <div className='bg-light'>
+        <div className='col w-100 border p-2 border-secondary rounded'>
+          <div className='row no-gutters align-items-center'>
+            <a href={"/profile/" + cafe.id}>
+              <img
+                className='border border-secondary rounded-circle mr-2'
+                src={cafe.profile_image}
+                width={64}
+                height={64}
+              />
+            </a>
+            <a href={"/profile/" + cafe.id}>
+              <div className='font-weight-bold'>{cafe.username}</div>
+            </a>
+          </div>
+
+          <div className='row no-gutters'></div>
+        </div>
+      </div>
+    );
+  }
 }
-
-
 
 export default Search;
